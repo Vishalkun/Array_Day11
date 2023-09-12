c24e6e58e7bfa8f24004661aced57adac3b2990f
Vishal github <Vishal.kunchal@gmail.com>
2023-09-12 14:12:23 +0530

    [vishal kunchal]: Array all basic problems

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+
+namespace ArrayDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] NumArr = new int[5];
+            NumArr[0] = 1;
+            NumArr[1] = 2;
+            NumArr[2] = 3;
+            NumArr[3] = 4;
+            NumArr[4] = 5;
+            Console.WriteLine(NumArr.Length);
+            foreach (int i in NumArr)
+            {
+                Console.WriteLine(i);
+            }
+           // Console.WriteLine("Hello, World!");
+           ArrayList arrayList = new ArrayList();   
+            arrayList.Add(1);       
+             arrayList.Add(2);
+
+            Console.WriteLine(arrayList.Count);
+            ArrayDuplicate.Duplicate();
+            UniqueElements.elements();
+        }
+       
+    }
+}
---
+++ UniqueElements.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDemo
+{
+    internal class UniqueElements
+    {
+        static public void elements()
+        {
+            int[] items = { 2, 3, 5, 3, 7, 5 };
+            int n = items.Length;
+
+            Console.WriteLine("Unique array elements: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                bool isDuplicate = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (items[i] == items[j])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+
+                if (!isDuplicate)
+                {
+                    Console.WriteLine(items[i]);
+
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+    }
+
